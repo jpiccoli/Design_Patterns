757abe7a34d9192b5adf8b0bb1df607e6ecd9be5
Joseph Piccoli <joe13676@comcast.net>
2020-07-04 13:39:35 -0700

    Implement Memento pattern
    Implement Memento pattern

--- include/Behavioral/Memento.h
+++ include/Behavioral/Memento.h
@@ -1,1 +1,71 @@
 #pragma once
+
+// https://refactoring.guru/design-patterns/memento/cpp/example#example-0
+
+#include <string>
+#include <ctime>
+#include <time.h>
+#include <vector>
+
+#include <iostream>
+
+#pragma warning(disable:4996)
+
+struct Memento
+{
+  virtual std::string GetName() const = 0;
+  virtual std::string date() const = 0;
+  virtual std::string state() const = 0;
+  virtual ~Memento() {}
+};
+
+struct ConcreteMemento : public Memento
+{
+  ConcreteMemento( std::string state )
+  {
+    this->state_ = state;
+    std::time_t now = std::time( 0 );
+    this->date_ = std::ctime( &now );
+  }
+
+  std::string state() const override { return this->state_; }
+
+  std::string GetName() const override
+  {
+    return this->date_ + " / ( " + this->state_.substr( 0, 9 ) + "...)";
+  }
+
+  std::string date() const override { return this->date_; }
+
+private:
+  std::string state_;
+  std::string date_;
+};
+
+struct Originator
+{
+  Originator( std::string state );
+  std::string GenerateRandomString( int length = 10 );
+  void DoSomething();
+  Memento* Save();
+  void Restore( Memento* memento );
+
+private:
+  std::string state_;
+};
+
+struct Caretaker
+{
+  Caretaker( Originator *originator ) : originator_( originator )
+  { 
+    this->originator_ = originator; 
+  }
+
+  void Backup();
+  void Undo();
+  void ShowHistory();
+
+private:
+  std::vector<Memento*> mementos_;
+  Originator * originator_;
+};
--- source/Behavioral/Memento.cpp
+++ source/Behavioral/Memento.cpp
@@ -1,2 +1,80 @@
+// Memento allows for capture and externalization of an objects internal state
+// so that object may be restored to this state later.
+
 #include "../../include/Behavioral/Memento.h"
 
+#include <iostream>
+
+Originator::Originator( std::string state ) : state_( state )
+{
+  std::cout << "Originator: My initial state is: " << this->state_ << "\n";
+}
+
+std::string Originator::GenerateRandomString( int length )
+{
+  const char alphanum[] =
+    "0123456789"
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz";
+  int stringLength = sizeof( alphanum ) - 1;
+
+  std::string random_string;
+  for( int i = 0; i < length; i++ )
+  {
+    random_string += alphanum[ std::rand() % stringLength ];
+  }
+  
+  return random_string;
+}
+
+void Originator::DoSomething()
+{
+  std::cout << "Originator: I'm doing something important.\n";
+  this->state_ = this->GenerateRandomString( 30 );
+  std::cout << "Originator: and my state has changed to: " << this->state_ << "\n";
+}
+
+Memento* Originator::Save()
+{ 
+  return new ConcreteMemento( this->state_ );
+
+}
+void Originator::Restore( Memento* memento )
+{
+  this->state_ = memento->state();
+}
+
+void Caretaker::Backup()
+{
+  std::cout << "\nCaretaker: Saving Originator's state...\n";
+  this->mementos_.push_back( this->originator_->Save() );
+}
+
+void Caretaker::Undo()
+{
+  if( !this->mementos_.size() )
+  {
+    std::cout << "Nothing to undo.\n";
+    return;
+  }
+  Memento *memento = this->mementos_.back();
+  this->mementos_.pop_back();
+  std::cout << "Caretaker: Restoring state to: " << memento->GetName() << "\n";
+  try 
+  {
+    this->originator_->Restore( memento );
+  }
+  catch( ... )
+  {
+    this->Undo();
+  }
+}
+
+void Caretaker::ShowHistory()
+{
+  std::cout << "Caretaker: Here's the list of mementos:\n";
+  for( Memento *memento : this->mementos_ )
+  {
+    std::cout << memento->GetName() << "\n";
+  }
+}
