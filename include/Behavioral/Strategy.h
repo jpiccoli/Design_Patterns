3216f25fba349440f0ace8184bb56626ca97308c
Joseph Piccoli <joe13676@comcast.net>
2020-07-04 12:53:52 -0700

    Implement code for Strategy pattern
    Implement code for Strategy pattern

--- include/Behavioral/Strategy.h
+++ include/Behavioral/Strategy.h
@@ -1,1 +1,44 @@
 #pragma once
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// https://refactoring.guru/design-patterns/strategy/cpp/example
+
+struct Strategy
+{
+  virtual ~Strategy() {}
+  virtual std::string DoAlgorithm( const std::vector<std::string> &data ) const = 0;
+};
+
+struct Context
+{
+
+  Context( Strategy* strategy = nullptr ) : strategy_( strategy )
+  {
+  }
+
+  ~Context()
+  {
+    delete this->strategy_;
+  }
+
+  void set_strategy( Strategy* strategy );
+
+  void DoSomeBusinessLogic( std::vector<std::string> &data ) const;
+
+private:
+  Strategy * strategy_;
+
+};
+
+struct ConcreteStrategyA : public Strategy
+{
+  std::string DoAlgorithm( const std::vector<std::string> &data ) const;
+};
+
+struct ConcreteStrategyB : public Strategy
+{
+  std::string DoAlgorithm( const std::vector<std::string> &data ) const;
+};
--- source/Behavioral/Strategy.cpp
+++ source/Behavioral/Strategy.cpp
@@ -1,3 +1,50 @@
 #include "../../include/Behavioral/Strategy.h"
 
+#include <iostream>
 
+// Strategy pattern lets the algorithm vary independently from clients that use it.
+
+void Context::set_strategy( Strategy* strategy )
+{
+  delete this->strategy_;
+  this->strategy_ = strategy;
+}
+
+void Context::DoSomeBusinessLogic(std::vector<std::string> &data) const
+{
+  std::cout << "Context: Sorting the data using the strategy (not sure how it'll do it)" << '\n';
+  std::string result = this->strategy_->DoAlgorithm( data );
+  std::cout << result << '\n';
+}
+
+std::string ConcreteStrategyA::DoAlgorithm( const std::vector<std::string> &data ) const
+{
+  std::cout << "ConcreteStrategyA::DoAlgorithm()\n";
+
+  std::string result;
+  std::for_each( std::begin( data ), std::end( data ), [ &result ]( const std::string &number ) {
+    result += number;
+  });
+
+  std::sort( std::begin( result ), std::end( result ) );
+
+  return result;
+}
+
+std::string ConcreteStrategyB::DoAlgorithm( const std::vector<std::string> &data ) const
+{
+  std::cout << "ConcreteStrategyB::DoAlgorithm()\n";
+
+  std::string result;
+  std::for_each( std::begin( data ), std::end( data ), [&result] ( const std::string &number ) {
+    result += number;
+  } );
+
+  std::sort( std::begin( result ), std::end( result ) );
+  for( unsigned i = 0; i < result.size() / 2; i++ )
+  {
+    std::swap( result[ i ], result[ result.size() - i - 1 ] );
+  }
+
+  return result;
+}
