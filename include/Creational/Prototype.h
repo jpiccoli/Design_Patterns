521ad9d9bd1f806ec8b19f53598c29e7ddc5df12
Joseph Piccoli <joe13676@comcast.net>
2020-08-09 15:14:45 -0700

    Implement code for Prototype pattern
    Implement code for Prototype pattern

--- include/Creational/Prototype.h
+++ include/Creational/Prototype.h
@@ -1,1 +1,64 @@
 #pragma once
+
+#include <string>
+#include <unordered_map>
+
+enum Designator { DESIGNATOR_1 = 0, DESIGNATOR_2 = 1 };
+
+struct Prototype
+{
+protected:
+  std::string prototype_name;
+  double prototype_field;
+
+public:
+  Prototype();
+
+  Prototype( std::string prototype_name_ );
+
+  virtual ~Prototype() {}
+
+  virtual Prototype *Clone() const = 0;
+
+  virtual void SetOperation( double prototype_field_ );
+};
+
+struct ConcretePrototype1 : public Prototype
+{
+private:
+  double concrete_prototype_field1;
+
+public:
+  Prototype* Clone() const override;
+
+  ConcretePrototype1( std::string prototype_name_, double concrete_prototype_field_ );
+
+};
+
+struct ConcretePrototype2 : public Prototype
+{
+private:
+  double concrete_prototype_field2;
+
+public:
+  Prototype * Clone() const override;
+
+  ConcretePrototype2( std::string prototype_name_, double concrete_prototype_field_ );
+
+};
+
+struct PrototypeFactory
+{
+private:
+  std::unordered_map<Designator, Prototype *, std::hash<int>> prototypes;
+
+public:
+  PrototypeFactory();
+
+  ~PrototypeFactory();
+
+  Prototype* CreatePrototype( Designator designator );
+  //{
+  //  return prototypes[ designator ]->Clone();
+  //}
+};
--- source/Creational/Prototype.cpp
+++ source/Creational/Prototype.cpp
@@ -1,3 +1,56 @@
 #include "../../include/Creational/Prototype.h"
 
+#include <iostream>
 
+// Provides for copying of existing objects without making your code dependent on their classes.
+
+Prototype::Prototype()
+{
+}
+
+Prototype::Prototype( std::string prototype_name_ ) : prototype_name( prototype_name_ )
+{
+}
+
+void Prototype::SetOperation( double prototype_field_ )
+{
+  prototype_field = prototype_field_;
+  std::cout << "Call SetOperation from " << prototype_name << " with " << prototype_field << '\n';
+}
+
+ConcretePrototype1::ConcretePrototype1( std::string prototype_name_, double concrete_prototype_field_ )
+  : Prototype( prototype_name_ ), concrete_prototype_field1( concrete_prototype_field_ )
+{
+}
+
+Prototype* ConcretePrototype1::Clone() const
+{
+  return new ConcretePrototype1(*this );
+}
+
+ConcretePrototype2::ConcretePrototype2( std::string prototype_name_, double concrete_prototype_field_ )
+  : Prototype( prototype_name_ ), concrete_prototype_field2( concrete_prototype_field_ )
+{
+}
+
+Prototype* ConcretePrototype2::Clone() const
+{
+  return new ConcretePrototype2( *this );
+}
+
+PrototypeFactory::PrototypeFactory()
+{
+  prototypes[ Designator::DESIGNATOR_1 ] = new ConcretePrototype1( "DESIGNATOR_1 ", 50.0 );
+  prototypes[ Designator::DESIGNATOR_2 ] = new ConcretePrototype2( "DESIGNATOR_2 ", 60.0 );
+}
+
+PrototypeFactory::~PrototypeFactory()
+{
+  delete prototypes[ Designator::DESIGNATOR_1 ];
+  delete prototypes[ Designator::DESIGNATOR_2 ];
+}
+
+Prototype* PrototypeFactory::CreatePrototype( Designator designator )
+{
+  return prototypes[ designator ]->Clone();
+}
