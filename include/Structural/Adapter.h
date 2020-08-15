4349480e6524d419f4f66e37741212ff143f278c
Joseph Piccoli <joe13676@comcast.net>
2020-08-15 10:50:21 -0700

    Implement code for Adapter pattern
    Implement code for Adapter pattern

--- include/Structural/Adapter.h
+++ include/Structural/Adapter.h
@@ -1,1 +1,48 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+struct BasicProngPlug // Target
+{
+private:
+  double radius;
+
+public:
+  BasicProngPlug() {}
+  BasicProngPlug( double radius_ ) : radius( radius_ ) {}
+  virtual double get_radius() const { return radius; }
+};
+
+struct BasicProngSocket
+{
+private:
+  double radius;
+
+public:
+  BasicProngSocket( double radius_ ) : radius( radius_ ) {}
+  double get_radius() const { return radius; }
+  bool fits( BasicProngPlug  const& plug ) const;
+};
+
+struct AdvancedProngPlug // Adaptee
+{
+private:
+  double width;
+
+public:
+  AdvancedProngPlug( double width_ ) : width( width_ ) {}
+  double get_width() const { return width; }
+};
+
+struct AdvancedProngPlugAdaptor : public BasicProngPlug
+{
+private:
+  AdvancedProngPlug advanced_prong_plug;
+
+public:
+  AdvancedProngPlugAdaptor( AdvancedProngPlug advanced_prong_plug_ ) : advanced_prong_plug( advanced_prong_plug_ ) {}
+  virtual double get_radius() const override;
+};
+
--- source/Structural/Adapter.cpp
+++ source/Structural/Adapter.cpp
@@ -1,3 +1,13 @@
+// A structural design pattern that allows objects with incompatible interfaces to collaborate.
+
 #include "../../include/Structural/Adapter.h"
 
+bool BasicProngSocket::fits( BasicProngPlug  const& plug ) const
+{ 
+  return this->get_radius() >= plug.get_radius();
+}
 
+double AdvancedProngPlugAdaptor::get_radius() const
+{
+  return advanced_prong_plug.get_width() * sqrt( 2 ) / 2;
+}
