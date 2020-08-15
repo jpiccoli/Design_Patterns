0e45f90c858849f4bd9a581535fbc260d9b48685
Joseph Piccoli <joe13676@comcast.net>
2020-08-15 12:36:09 -0700

    Implement code for Composite pattern
    Implement code for Composite pattern

--- include/Structural/Composite.h
+++ include/Structural/Composite.h
@@ -1,1 +1,40 @@
 #pragma once
+
+#include <iostream>
+#include <list>
+#include <memory>
+#include <string>
+
+struct Component
+{
+protected:
+  std::shared_ptr<Component> parent;
+
+public:
+  virtual ~Component() {};
+  void set_parent(std::shared_ptr<Component> parent_) { parent = parent_; }
+  std::shared_ptr<Component> get_parent() const { return parent; }
+
+  virtual void add(std::shared_ptr<Component>) {}
+  virtual void remove(std::shared_ptr<Component>) {}
+  virtual bool is_composite() const { return false; }
+  virtual std::string operation() const = 0;
+};
+
+struct Leaf : public Component
+{
+  std::string operation() const override;
+};
+
+struct Composite : public Component
+{
+protected:
+  std::list<std::shared_ptr<Component>> elements;
+
+public:
+  void add(std::shared_ptr<Component>);
+  void remove(std::shared_ptr<Component>);
+  bool is_composite() const { return true; }
+  std::string operation() const;
+};
+
--- source/Structural/Composite.cpp
+++ source/Structural/Composite.cpp
@@ -1,2 +1,41 @@
+// A structural design pattern that lets you compose objects into tree structures and then work 
+// with these structures as if they were individual objects.
+
 #include "../../include/Structural/Composite.h"
+
+std::string Leaf::operation() const
+{
+  return "Leaf";
+}
+
+void Composite::add(std::shared_ptr<Component> component)
+{
+  elements.push_back(component);
+  component->set_parent(std::make_shared<Composite>(*this));
+}
+
+void Composite::remove(std::shared_ptr<Component> component)
+{
+  elements.remove(component);
+  component->set_parent(nullptr);
+}
+
+std::string Composite::operation() const
+{
+  std::string result;
+
+  for(auto const c : elements)
+  {
+    if(c == elements.back())
+    {
+      result += c->operation();
+    }
+    else
+    {
+      result += c->operation() + "+";
+    }
+  }
+
+  return "Branch(" + result + ")";
+}
 
