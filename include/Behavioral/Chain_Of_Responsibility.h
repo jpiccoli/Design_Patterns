7775de8b2b827742cea435245d175115404ad4cb
Joseph Piccoli <joe13676@comcast.net>
2020-08-22 10:35:40 -0700

    Implement code for Chain of Responsibility pattern
    Implement code for Chain of Responsibility pattern

--- include/Behavioral/Chain_Of_Responsibility.h
+++ include/Behavioral/Chain_Of_Responsibility.h
@@ -1,1 +1,53 @@
 #pragma once
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+struct Handler
+{
+  virtual std::shared_ptr<Handler> SetNext(std::shared_ptr<Handler> handler) = 0;
+  virtual std::string Handle(std::string const& request) = 0;
+};
+
+struct BaseHandler : public Handler
+{
+private:
+  std::shared_ptr<Handler> next_handler;
+
+public:
+  BaseHandler() : next_handler(nullptr) {}
+  std::shared_ptr<Handler> SetNext(std::shared_ptr<Handler> handler_) override;
+  std::string Handle(std::string const& request) override;
+};
+
+struct CheckHandler : public BaseHandler
+{
+  std::string Handle(std::string const& request_) override;
+};
+
+struct CreditHandler : public BaseHandler
+{
+  std::string Handle(std::string const& request_) override;
+};
+
+struct CashHandler : public BaseHandler
+{
+  std::string Handle(std::string const& request_) override;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- source/Behavioral/Chain_Of_Responsibility.cpp
+++ source/Behavioral/Chain_Of_Responsibility.cpp
@@ -1,2 +1,45 @@
+// A behavioral design pattern that lets you pass requests along a chain of handlers. Upon receiving a 
+// request, each handler decides either to process the request or to pass it to the next handler in 
+// the chain.
+
 #include "../../include/Behavioral/Chain_Of_Responsibility.h"
+
+std::shared_ptr<Handler> BaseHandler::SetNext(std::shared_ptr<Handler> handler_)
+{
+  this->next_handler = handler_;
+  return handler_;
+}
+
+std::string BaseHandler::Handle(std::string const& request)
+{
+  if(this->next_handler)
+    return this->next_handler->Handle(request);
+
+  return "";
+}
+
+std::string CheckHandler::Handle(std::string const& request_)
+{
+  if(request_ == "WriteCheck")
+    return "CheckHandler: Doing operation " + request_ + ".\n";
+  else
+    return BaseHandler::Handle(request_);
+}
+
+std::string CreditHandler::Handle(std::string const& request_)
+{
+  if(request_ == "InputCreditCard")
+    return "CreditHandler: Doing operation " + request_ + ".\n";
+  else
+    return BaseHandler::Handle(request_);
+}
+
+std::string CashHandler::Handle(std::string const& request_)
+{
+  if(request_ == "PayWithCash")
+    return "CashHandler: Doing operation " + request_ + ".\n";
+  else
+    return BaseHandler::Handle(request_);
+}
+
 
