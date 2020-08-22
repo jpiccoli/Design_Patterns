eec2d583ec2b57096afca115b0dc2b0a1d494011
Joseph Piccoli <joe13676@comcast.net>
2020-08-22 09:55:58 -0700

    Implement code for Mediator pattern
    Implement code for Mediator pattern

--- include/Behavioral/Mediator.h
+++ include/Behavioral/Mediator.h
@@ -1,1 +1,46 @@
 #pragma once
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+struct BaseComponent;
+
+struct Mediator
+{
+  virtual void Notify(std::shared_ptr<BaseComponent> component, std::string const& event) const = 0;
+};
+
+struct BaseComponent
+{
+protected:
+  std::shared_ptr<Mediator> mediator;
+
+public:
+  BaseComponent(std::shared_ptr<Mediator> mediator_ = nullptr) : mediator(mediator_) {}
+  void set_mediator(std::shared_ptr<Mediator> mediator);
+};
+
+struct Component1 : public BaseComponent
+{
+  void DoA();
+  void DoB();
+};
+
+struct Component2 : public BaseComponent
+{
+  void DoC();
+  void DoD();
+};
+
+struct ConcreteMediator : public Mediator
+{
+private:
+  std::shared_ptr<Component1> component1;
+  std::shared_ptr<Component2> component2;
+
+public:
+  ConcreteMediator() {}
+  ConcreteMediator(std::shared_ptr<Component1> component1_, std::shared_ptr<Component2> component2_);
+  void Notify(std::shared_ptr<BaseComponent> component, std::string const& event) const override;
+};
--- source/Behavioral/Mediator.cpp
+++ source/Behavioral/Mediator.cpp
@@ -1,2 +1,56 @@
+// A behavioral design pattern that lets you reduce chaotic dependencies between objects. The pattern 
+// restricts direct communications between the objects and forces them to collaborate only via a mediator 
+// object.
+
 #include "../../include/Behavioral/Mediator.h"
 
+void BaseComponent::set_mediator(std::shared_ptr<Mediator> mediator_)
+{
+  this->mediator = mediator_;
+}
+
+void Component1::DoA()
+{
+  std::cout << "Component 1 does A\n";
+  this->mediator->Notify(std::make_shared<Component1>(*this), "A");
+}
+
+void Component1:: DoB()
+{
+  std::cout << "Component 1 does B\n";
+  this->mediator->Notify(std::make_shared<Component1>(*this), "B");
+}
+
+void Component2::DoC()
+{
+  std::cout << "Component 2 does C\n";
+  this->mediator->Notify(std::make_shared<Component2>(*this), "C");
+}
+
+void Component2::DoD()
+{
+  std::cout << "Component 2 does D\n";
+  this->mediator->Notify(std::make_shared<Component2>(*this), "D");
+}
+
+ConcreteMediator::ConcreteMediator(std::shared_ptr<Component1> component1_, std::shared_ptr<Component2> component2_)
+  : component1(component1_), component2(component2_)
+{
+  this->component1->set_mediator(std::make_shared<ConcreteMediator>(*this));
+  this->component2->set_mediator(std::make_shared<ConcreteMediator>(*this));
+}
+
+void ConcreteMediator::Notify(std::shared_ptr<BaseComponent> component, std::string const& event) const
+{
+  if(event == "A")
+  {
+    std::cout << "Mediator reacts on A and triggers the following operations:\n";
+    component2->DoC();
+  }
+  if(event == "D")
+  {
+    std::cout << "Mediator reacts on D and triggers the following operations:\n";
+    component1->DoB();
+    component2->DoC();
+  }
+}
