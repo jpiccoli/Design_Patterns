7c67f18f5125c028e5d9ae8869cf6ac33e9dc409
Joseph Piccoli <joe13676@comcast.net>
2020-08-29 12:46:12 -0700

    Implement code for Template Method pattern
    Implement code for Template Method pattern

--- include/Behavioral/Template_Method.h
+++ include/Behavioral/Template_Method.h
@@ -1,1 +1,44 @@
 #pragma once
+
+#include <iostream>
+#include <list>
+#include <memory>
+#include <string>
+#include <vector>
+
+struct BasicClass
+{
+protected:
+  void BaseOperation1() const;
+  void BaseOperation2() const;
+  void BaseOperation3() const;
+
+  virtual void RequiredOperations1() const = 0;
+  virtual void RequiredOperations2() const = 0;
+
+  virtual void ExtraOperations1() const;
+  virtual void ExtraOperations2() const;
+
+public:
+  void TemplateMethod() const;
+};
+
+struct ActionClass1 : public BasicClass
+{
+protected:
+  void RequiredOperations1() const override;
+  void RequiredOperations2() const override;
+  void ExtraOperations1() const override;
+};
+
+struct ActionClass2 : public BasicClass
+{
+  void RequiredOperations1() const override;
+  void RequiredOperations2() const override;
+  void ExtraOperations2() const override;
+};
+
+
+
+
+
--- source/Behavioral/Template_Method.cpp
+++ source/Behavioral/Template_Method.cpp
@@ -1,2 +1,70 @@
+// A behavioral design pattern that defines the skeleton of an algorithm in the superclass 
+// but lets subclasses override specific steps of the algorithm without changing its structure.
+
 #include "../../include/Behavioral/Template_Method.h"
 
+void BasicClass::BaseOperation1() const
+{
+  std::cout << "BaseClass: BaseOperation1\n";
+}
+
+void BasicClass::BaseOperation2() const
+{
+  std::cout << "BaseClass: BaseOperation2\n";
+}
+
+void BasicClass::BaseOperation3() const
+{
+  std::cout << "BaseClass: BaseOperation3\n";
+}
+
+void BasicClass::ExtraOperations1() const
+{
+  std::cout << "BaseClass: ExtraOperation1\n";
+}
+
+void BasicClass::ExtraOperations2() const
+{
+  std::cout << "BaseClass: ExtraOperation2\n";
+}
+
+void BasicClass::TemplateMethod() const
+{
+  this->BaseOperation1();
+  this->RequiredOperations1();
+  this->BaseOperation2();
+  this->ExtraOperations1();
+  this->RequiredOperations2();
+  this->BaseOperation3();
+  this->ExtraOperations2();
+}
+
+void ActionClass1::RequiredOperations1() const
+{
+  std::cout << "ActionClass1: RequiredOperations1\n";
+}
+
+void ActionClass1::RequiredOperations2() const
+{
+  std::cout << "ActionClass1: RequiredOperations2\n";
+}
+
+void ActionClass1::ExtraOperations1() const
+{
+  std::cout << "ActionClass1: ExtraOperations1\n";
+}
+
+void ActionClass2::RequiredOperations1() const
+{
+  std::cout << "ActionClass2: RequiredOperations1\n";
+}
+
+void ActionClass2::RequiredOperations2() const
+{
+  std::cout << "ActionClass2: RequiredOperations1\n";
+}
+
+void ActionClass2::ExtraOperations2() const
+{
+  std::cout << "ActionClass2: ExtraOperations2\n";
+}
