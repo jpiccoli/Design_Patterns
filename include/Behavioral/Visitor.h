750c6cab08eb8c7d6fc0400dc5ee40a20228fc91
Joseph Piccoli <joe13676@comcast.net>
2020-09-05 10:02:50 -0700

    Implement code for Visitor pattern
    Implement code for Visitor pattern

--- include/Behavioral/Visitor.h
+++ include/Behavioral/Visitor.h
@@ -1,1 +1,71 @@
 #pragma once
+
+#include <array>
+#include <iostream>
+#include <memory>
+#include <string>
+
+struct ConcreteComponentA;
+struct ConcreteComponentB;
+
+struct Visitor
+{
+  virtual void VisitConcreteComponentA(const std::shared_ptr<ConcreteComponentA> element) const = 0;
+  virtual void VisitConcreteComponentB(const std::shared_ptr<ConcreteComponentB> element) const = 0;
+};
+
+struct Visitor_Component
+{
+  virtual ~Visitor_Component() {}
+  virtual void Accept(const std::shared_ptr<Visitor> visitor) const = 0;
+};
+
+struct ConcreteComponentA : public Visitor_Component
+{
+  void Accept(const std::shared_ptr<Visitor> visitor) const override;
+  std::string ExclusiveComponentA() const;
+};
+
+struct ConcreteComponentB : public Visitor_Component
+{
+  void Accept(const std::shared_ptr<Visitor> visitor) const override;
+  std::string ExclusiveComponentB() const;
+};
+
+struct ConcreteVisitor1 : public Visitor
+{
+  void VisitConcreteComponentA(const std::shared_ptr<ConcreteComponentA> element_) const override;
+  void VisitConcreteComponentB(const std::shared_ptr<ConcreteComponentB> element_) const override;
+};
+
+struct ConcreteVisitor2 : public Visitor
+{
+  void VisitConcreteComponentA(const std::shared_ptr<ConcreteComponentA> element_) const override;
+  void VisitConcreteComponentB(const std::shared_ptr<ConcreteComponentB> element_) const override;
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//class ConcreteComponentA1;
+//class ConcreteComponentB1;
+//
+//class Visitor1 {
+//public:
+//  virtual void VisitConcreteComponentA1(const ConcreteComponentA1 *element) const = 0;
+//  virtual void VisitConcreteComponentB1(const ConcreteComponentB1 *element) const = 0;
+//};
+//
+//class Component1 {
+//public:
+//  virtual ~Component1() {}
+//  virtual void Accept(Visitor1 *visitor) const = 0;
+//};
+//
+//class ConcreteComponentA1 : public Component1 {
+//public:
+//  void Accept(Visitor1 *visitor) const override {
+//    visitor->VisitConcreteComponentA1(this);
+//  }
+//  std::string ExclusiveMethodOfConcreteComponentA() const {
+//    return "A";
+//  }
+//};
--- source/Behavioral/Visitor.cpp
+++ source/Behavioral/Visitor.cpp
@@ -1,2 +1,43 @@
+// A behavioral design pattern that lets you separate algorithms from the objects on which they operate.
+
 #include "../../include/Behavioral/Visitor.h"
 
+void ConcreteComponentA::Accept(const std::shared_ptr<Visitor> visitor) const
+{
+  visitor->VisitConcreteComponentA(std::make_shared<ConcreteComponentA>(*this));
+}
+
+std::string ConcreteComponentA::ExclusiveComponentA() const
+{
+  return "A";
+}
+
+void ConcreteComponentB::Accept(const std::shared_ptr<Visitor> visitor) const
+{
+  visitor->VisitConcreteComponentB(std::make_shared<ConcreteComponentB>(*this));
+}
+
+std::string ConcreteComponentB::ExclusiveComponentB() const
+{
+  return "B";
+}
+
+void ConcreteVisitor1::VisitConcreteComponentA(const std::shared_ptr<ConcreteComponentA> element) const
+{
+  std::cout << element->ExclusiveComponentA() << " + ConcreteVisitor1\n";
+}
+
+void ConcreteVisitor1::VisitConcreteComponentB(const std::shared_ptr<ConcreteComponentB> element) const
+{
+  std::cout << element->ExclusiveComponentB() << " + ConcreteVisitor1\n";
+}
+
+void ConcreteVisitor2::VisitConcreteComponentA(const std::shared_ptr<ConcreteComponentA> element) const
+{
+  std::cout << element->ExclusiveComponentA() << " + ConcreteVisitor2\n";
+}
+
+void ConcreteVisitor2::VisitConcreteComponentB(const std::shared_ptr<ConcreteComponentB> element) const
+{
+  std::cout << element->ExclusiveComponentB() << " + ConcreteVisitor2\n";
+}
